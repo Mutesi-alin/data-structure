013cd0e38c6cab49d9725ac42415fdc298bfa32b
Student <mutesi-alin>
2024-03-31 15:27:51 +0300

    add files

---
+++ stack.py
@@ -0,0 +1,15 @@
+st1=[]
+st1.append(300)
+st1.append(46)
+print(st1)
+
+
+stack= [9,7,8,9,5,]
+stack.pop()
+print(stack)
+
+name=["aline","mutesi","nancy","fanny"]
+name.remove("aline")
+print(name)
+name.pop()
+print(name)
